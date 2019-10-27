ee456b124df1388ee9d666baa86dc2e624002f55
Kolton Chiu <kchiu21@ssis.edu.vn>
2019-10-27 13:41:13 +0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,8 @@
 """
 Kolton Chiu
 This programs prints the initial letters of the users input.
+https://www.w3schools.com/python/ref_string_join.asp
+https://www.w3schools.com/python/ref_string_upper.asp
 """
 #Get user's full name
 user_name = input("")
@@ -9,8 +11,6 @@ user_name = input("")
 user_name = user_name.split()
 
 #Get first letter of name & make it upper case
-#https://www.w3schools.com/python/ref_string_join.asp
-#https://www.w3schools.com/python/ref_string_upper.asp
 initial_upper = [i[0].upper() for i in user_name]
 final_initial = "".join(initial_upper)
 print (final_initial + "\n") 
